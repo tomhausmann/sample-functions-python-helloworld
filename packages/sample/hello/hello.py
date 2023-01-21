57f19aeda86462247863a06ebe579460d1109cb3
tomhausmann <tom.hausmann@gmail.com>
2023-01-21 15:55:25 +0000

    another update

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,9 +1,9 @@
 import os
 
 def main(args):
-      URL=os.environ["URL"]
+      print(os.environ["URL"])
       name = args.get("name", "stranger")
-      greeting = "Hello " + name + "!!!!" + URL
+      greeting = "Hello " + name + "!!!!"
       print(greeting)
       return {"body": greeting}
   
