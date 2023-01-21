9205db059c802949a72765dc53460fae7dfb02eb
tomhausmann <tom.hausmann@gmail.com>
2023-01-21 18:54:26 +0000

    localtime

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -5,6 +5,6 @@ TEST=os.getenv("TEST")
 
 def main(args):
       name = args.get("name", "stranger")
-      greeting = "Hello " + name + "!!!!" + TEST + " " + time.localtime()
+      greeting = "Hello " + name + "!!!!" + TEST + " " + time.localtime(0)
       print(greeting)
       return {"body": greeting}
