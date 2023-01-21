1085cad61adf01657a4e6904d1244175e886ae99
tomhausmann <tom.hausmann@gmail.com>
2023-01-21 18:16:42 +0000

    test

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,7 +1,8 @@
 import os
 
+TEST=os.system('$TEST')
+
 def main(args):
-      TEST=os.system('$TEST')
       name = args.get("name", "stranger")
       greeting = "Hello " + name + "!!!!" + TEST
       print(greeting)
